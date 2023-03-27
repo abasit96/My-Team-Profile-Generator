db60536d91467893ed26239491a3cce22685682f
Bootcamp <bootcamp@Abduls-MacBook-Air.local>
2023-03-27 05:05:58 -0700

    added html so node index functions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateTeam = require("./src/page-template.js");
+// const generateTeam = require("./src/page-template.js");
 
 // lib modules
 const Engineer = require("./lib/Engineer");
@@ -8,11 +8,11 @@ const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
 // Array for answers to questions
-const newStaffMemberData = [];
+const teamMembers = [];
 
 // Array object questions asked in CLI to user
-function addManager() {
-    inquirer
+async function addManager() {
+    await inquirer
         .prompt([
             {
                 type: 'input',
@@ -37,13 +37,13 @@ function addManager() {
         ])
         .then(response => {
             const manager = new Manager(response.name, response.id, response.email, response.officeNumber)
+            console.log(manager);
             teamMembers.push(manager)
         })
-        .then(() => init())
 }
 
-function addEngineer() {
-    inquirer
+async function addEngineer() {
+    await inquirer
         .prompt([
             {
                 type: 'input',
@@ -68,13 +68,13 @@ function addEngineer() {
         ])
         .then(response => {
             const engineer = new Engineer(response.name, response.id, response.email, response.github)
+            console.log(engineer);
             teamMembers.push(engineer)
         })
-        .then(() => init())
 }
 
-function addIntern() {
-    inquirer
+async function addIntern() {
+    await inquirer
         .prompt([
             {
                 type: 'input',
@@ -99,39 +99,70 @@ function addIntern() {
         ])
         .then(response => {
             const intern = new Intern(response.name, response.id, response.email, response.school)
+            console.log(intern);
             teamMembers.push(intern)
         })
-        .then(() => init())
 }
-async function promptQuestions() {
-  await questions()
-    
-  
-  const addMemberAns = await inquirer
-    .prompt([
-      {
-        name:'addMember',
-        type: 'list',
-        choices: ['Add a new member', 'Create team'],
-        message: "What would you like to do next?"
-      }
-    ])
 
-    if (addMemberAns.addMember === 'Add a new member') {
-      return promptQuestions()
+
+async function start() {
+
+    console.log('Welcome, Please add a Manager to Continue forming your team\n');
+    await addManager();
+
+    let addMemberAns = await inquirer
+        .prompt([
+            {
+                name: 'addMember',
+                type: 'list',
+                choices: ['Add a new member', 'Finish Team Creation'],
+                message: "What would you like to do next?"
+            }
+        ])
+
+    while (addMemberAns.addMember === 'Add a new member') {
+        let memberType = await inquirer
+            .prompt([
+                {
+                    name: 'memberType',
+                    type: 'list',
+                    choices: ['Engineer', 'Intern'],
+                    message: "What type of Team Member would you like to add?"
+                }
+            ])
+
+        if (memberType.memberType === 'Engineer') {
+            await addEngineer();
+        }
+        else if (memberType.memberType === 'Intern') {
+            await addIntern()
+        }
+
+        addMemberAns = await inquirer
+            .prompt([
+                {
+                    name: 'addMember',
+                    type: 'list',
+                    choices: ['Add a new member', 'Finish Team Creation'],
+                    message: "What would you like to do next?"
+                }
+            ])
     }
-    return createTeam();
-}  
+
+    if (addMemberAns.addMember === 'Finish Team Creation') {
+        console.log('Team Created', teamMembers);
+    }
+}
 
 function createFile() {
     if (!fs.existsSync(OUTPUT_DIR)) {
-      fs.mkdirSync(OUTPUT_DIR);
+        fs.mkdirSync(OUTPUT_DIR);
     } else {
-  
-      fs.writeFileSync(outputPath, render(teamMembers), "UTF-8");
-      console.log("File created in the output folder");
+
+        fs.writeFileSync(outputPath, render(teamMembers), "UTF-8");
+        console.log("File created in the output folder");
     }
-    
-  }
-  
-  start();
+
+}
+
+start();
